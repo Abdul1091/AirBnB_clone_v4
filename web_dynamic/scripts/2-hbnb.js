e2a8ac333baca656735b02be16f0a253554057ea
Abdul1091 <Abdul2kur1@gmail.com>
2024-08-11 14:50:15 -0700

    API status

---
+++ web_dynamic/scripts/2-hbnb.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    function updateApiStatus() {
+        $.get("http://0.0.0.0:5001/api/v1/status/", function(data) {
+            if (data.status === "OK") {
+                $('#api_status').addClass('available');
+            } else {
+                $('#api_status').removeClass('available');
+            }
+        }).fail(function() {
+            $('#api_status').removeClass('available');
+        });
+    }
+
+    updateApiStatus();
+    setInterval(updateApiStatus, 5000); // Check API status every 5 seconds
+});
